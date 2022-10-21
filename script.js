b0cfaab2e56d5e0d519564947d6a21d10332a9f2
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-10-21 11:38:13 +0300

    Added drag&drop funcs

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 const item = document.querySelector(".item")
+const placeholders = document.querySelectorAll(".placeholder")
+
+const dragover = (event) => {
+  event.preventDefault()
+}
+const dragenter = (event) => {
+  event.target.classList.add("hovered")
+}
+const dragleave = (event) => {
+  event.target.classList.remove("hovered")
+}
+const drop = (event) => {
+  event.target.classList.remove("hovered")
+  event.target.append(item) 
+}
+
+placeholders.forEach((placeholder)=>{
+  placeholder.addEventListener("dragover", dragover)
+  placeholder.addEventListener("dragenter", dragenter)
+  placeholder.addEventListener("dragleave", dragleave)
+  placeholder.addEventListener("drop", drop)
+})
+
 console.log(item);
 const dragstart = (event) => {
   event.target.classList.add("hold")
